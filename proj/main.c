40ed0b80b4719881411dce5137f94c026e0c9e62
Alexander Salois <asalois.scholar@gmail.com>
2021-11-08 17:15:18 -0700

    something is not working

--- proj/main.c
+++ proj/main.c
@@ -1,52 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <math.h>
 
 //#define size 64 //2^6
 //#define size 128 //2^7
-//#define size 1024 //2^10
+#define size 1024 //2^10
 //#define size 4096 //2^12
-#define size 1048576 //2^20
+//#define size 1048576 //2^20
 //#define size 2097252 //2^21
 //#define size 268435456 //2^28
 //#define size 536870912 //2^29
 //#define size 1073741824 //2^30
 
-const int M = 16;
+struct signal
+{
+    int sym[size];
+    double mod[size];
+    double noise[size];
+    double rx[size];
+};
 
-int *syms;
-double *md;
-double *rx;
-double *noise;
+const int M = 4;
+const int runNum = 16;
 
-int makeSeq(){ // make a sequence of data from 0 to M-1 
-    for(int i=0; i<size; i++){
-        syms[i] = rand()%M;
+struct signal makeSeq(struct signal sig)
+{ // make a sequence of data from 0 to M-1
+    for (int i = 0; i < size; i++)
+    {
+        sig.sym[i] = rand() % M;
     }
-    return 0;
+    return sig;
 }
 
-int pamMod(){ // pam mod the data to be [0-1]
-    for(int i=0; i<size; i++){
-        md[i] =  ((double)syms[i] / (double)(M-1));
+struct signal pamMod(struct signal sig)
+{ // pam mod the data to be [0-1]
+    for (int i = 0; i < size; i++)
+    {
+        sig.mod[i] = ((double)sig.sym[i] / (double)(M - 1));
     }
-    return 0;
-} 
-
-int pamDemod(){ // pam demod the data to be [0-1]
-    for(int i=0; i<size; i++){
-        rx[i] = (md[i] + noise[i]) * (double)(M-1);
-    }
-    return 0;
-} 
-
-double getBER(){
-    int err = 0;
-    for(int i=0; i<size; i++){
-        err += ((int)round(rx[i]))^syms[i];
-    }
-    double ber = (double)err / ((double)size*log2(M)) ;
-    return ber;
+    return sig;
 }
 
 double randn(double mu, double sigma)
@@ -77,23 +70,53 @@ double randn(double mu, double sigma)
     return (mu + sigma * (double)X1);
 }
 
-int makeNoise(double sig)
+struct signal makeNoise(struct signal sig, double sigma)
 { // add gausian noise
     for (int i = 0; i < size; i++)
     {
-        noise[i] = randn(0.0, sig);
+        sig.noise[i] = randn(0.0, sigma);
     }
-    return 0;
+    return sig;
 }
 
-double getSNR()
+struct signal pamDemod(struct signal sig)
+{ // pam demod the data to be [0, M-1]
+    for (int i = 0; i < size; i++)
+    {
+        sig.rx[i] = (sig.mod[i] + sig.noise[i]) * (double)(M - 1);
+    }
+    return sig;
+}
+
+uint64_t getErr(struct signal sig, uint64_t prevErrs)
+{
+    uint64_t err = prevErrs;
+    for (int i = 0; i < size; i++)
+    {
+        uint8_t xored = ((uint8_t)round(sig.rx[i])) ^ (uint8_t)sig.sym[i];
+        // Iterate through all the bits
+        while (xored > 0)
+        {
+            // If current bit is 1
+            if (xored & 1)
+            {
+                err++;
+            }
+
+            xored = xored >> 1;
+        }
+    }
+    return err;
+}
+
+double getSNR(struct signal sig)
 { // find snr
     double sumSig = 0;
     double sumN = 0;
     for (int i = 0; i < size; i++)
     {
-        sumSig += md[i];
-        sumN += noise[i];
+        sumSig += sig.mod[i];
+        sumN += sig.noise[i];
     }
     double aveSig = sumSig / size;
     double aveN = sumN / size;
@@ -102,17 +125,24 @@ double getSNR()
     return snr;
 }
 
-int main(){
-    syms = malloc(size*sizeof(int));
-    md = malloc(size*sizeof(double));
-    noise = malloc(size*sizeof(double));
-    rx = malloc(size*sizeof(double));
-    makeSeq();
-    pamMod();
-    makeNoise(0.01);
-    double snr = getSNR();
-    pamDemod();
-    double BER = getBER();
-    printf("BER = %f at %f dB SNR\n",BER,snr);
+int main()
+{
+    uint64_t err = 0;
+    struct signal sig;
+    for (int i = 0; i < runNum; i++)
+    {
+        sig = makeSeq(sig);
+        sig = pamMod(sig);
+        sig = makeNoise(sig, 0.08);
+        sig = pamDemod(sig);
+        err += getErr(sig, err);
+    }
+    double snr = getSNR(sig);
+    int total = size * runNum;
+    int bottom = total * 8;
+    double BER = (double)err / (double)bottom;
+    printf("errs = %d \nTotalSims = %d \nTotalBits = %d\n", err, total, bottom);
+    printf("BER = %f at %f dB SNR\n", BER, snr);
+
     return 0;
 }
