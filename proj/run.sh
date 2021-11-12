8c2592e84fc9ba041d37eb708a1873117aec87e3
Alex Salois <asalois.scholar@gmail.com>
2021-11-12 12:55:50 -0700

    fixed some naming

--- proj/run.sh
+++ proj/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script to compile and run proj
-gcc -o main main.c -lm -Wall
-time ./main > out.txt
+gcc -o runMain main.c -lm -Wall
+(time ./runMain) > out.txt 2>&1
 #cat out.txt
 #./find_errs.sh
