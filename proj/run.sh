0c0d15c602732d49e7d0ffcb1929c8c3fd6e209b
Alexander Salois <asalois.scholar@gmail.com>
2021-12-03 15:18:05 -0700

    make executables .o files

--- proj/run.sh
+++ proj/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script to compile and run proj
-gcc -o runMain main.c -lm -Wall
-(time ./runMain) > out.txt 2>&1
+gcc -lm -Wall -o main.o main.c
+(time ./main.o) > out.txt 2>&1
 #cat out.txt
 #./find_errs.sh
--- proj/run_par.sh
+++ proj/run_par.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script to compile and run proj
-gcc -pthread -o runParMain par_main.c -lm -Wall
-(time ./runParMain) > out.txt 2>&1
+gcc -lm -Wall -pthread -o par_main.o  par_main.c
+(time ./par_main.o) > out.txt 2>&1
 #cat out.txt
 #./find_errs.sh
