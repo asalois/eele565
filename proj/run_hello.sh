020ea797211ef27dc40756c9cc7269917d3cb959
Alexander Salois <asalois.scholar@gmail.com>
2021-12-03 12:16:08 -0700

    doing some work with pointers

--- proj/run_hello.sh
+++ proj/run_hello.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script to compile and run proj
-gcc -pthread -lm -Wall -o hello.o hello32.c &&  time ./hello.o
+gcc -pthread -lm -Wall -o hello.o hello_arg2.c &&  time ./hello.o
 #(time ./hello.o) > out_hello.txt 2>&1
