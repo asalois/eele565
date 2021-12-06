351ded15d009246d9503678aa881c9b3b5c1f451
Alexander Salois <asalois.scholar@gmail.com>
2021-12-06 15:45:34 -0700

    time no longer needed

--- proj/run_par.sh
+++ proj/run_par.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # script to compile and run proj
-gcc -lm -Wall -pthread -o par_main.o  par_main.c && time ./par_main.o
+gcc -lm -Wall -pthread -o par_main.o  par_main.c && ./par_main.o
 #(time ./par_main.o) > out.txt 2>&1
-#cat out.txt
-#./find_errs.sh
